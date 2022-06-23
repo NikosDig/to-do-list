5666bd77f7fd5ae12b9b96567125f73e9944c40a
NikosDigalakis <nikos_23_chania@hotmail.com>
2022-06-23 19:55:49 +0200

    semi fixed it
    added delete button but edit dont work

--- to do app/script.js
+++ to do app/script.js
@@ -1,25 +1,55 @@
 const form = document.querySelector("form");
-const title = document.querySelector(".titleOfApp");
+const titleOfTheApp = document.getElementById("titleOfApp");
 const addTask = document.querySelector(".add");
 const mainTaskContainer = document.querySelector(".mainTaskContainer");
+const taskListElement = document.querySelector(".taskList");
 
-form.addEventListener("submit", conlog);
+form.addEventListener("submit", addContent);
 
-function addContent() {
+function addContent(e) {
+  e.preventDefault();
   mainTaskContainer.innerHTML += `
     <div class="taskContainer">
     <div class="taskList">
-      <input type="text" value="" readonly />
+      <input type="text" value="${titleOfTheApp.value}" id="createdTask" readonly />
     </div>
     <div class="editDelete">
       <button class="edit">Edit</button>
-      <button class="delete">Delete</button>
+      <button onClick="deletePost(this)" class="delete">Delete</button>
     </div>
   </div>
     `;
+  titleOfTheApp.value = "";
+  const deleteButton = document.querySelector(".delete");
+  const editButton = document.querySelector(".edit");
+  const createdTask = document.getElementById("createdTask");
+  editButton.addEventListener("click", (e) => {
+    if (editButton.innerText == "Edit") {
+      editButton.innerText = "Save";
+      createdTask.removeAttribute("readonly");
+      createdTask.focus();
+    } else {
+      editButton.innerText = "Edit";
+      createdTask.setAttribute("readonly", "readonly");
+    }
+  });
 }
 
-function conlog(e) {
-  e.preventDefault();
-  console.log(e.target);
+function deletePost(e) {
+  e.parentElement.parentElement.remove();
 }
+
+// function editPost(e) {
+// createdTask.removeAttribute("readonly");
+// console.log(e.taskListElement.firstChild);
+// }
+// function editPost(e) {
+//   if (editButton.innerText == "Edit") {
+//     editButton.innerText = "Save";
+//     createdTask.removeAttribute("readonly");
+//     createdTask.focus();
+//   } else {
+//     editButton.innerText = "Edit";
+//     createdTask.setAttribute("readonly", "readonly");
+//   }
+// }
